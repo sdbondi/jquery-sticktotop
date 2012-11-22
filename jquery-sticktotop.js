ac5e8f4aa798889020f06596416c44ad35384bc3
Larry Botha <larrywp@themousepotatowebsite.co.za>
2012-11-22 07:00:33 -0500

    updates to sticktotop for more generic use, update examples

--- jquery-sticktotop.js
+++ jquery-sticktotop.js
@@ -1,128 +1,183 @@
 /*global window */
 (function(document, $) {
-  "use strict";
+    "use strict";
 
-  $.fn.stickToTop = function(options) {
-    options = $.extend({
-      scrollParent: window,
-      offset: {top: 0, left: 0},
-      minParentHeight: false,
-      minParentWidth: false,
-      bottomBound: false,
-      onStick: null,
-      onDetach: null
-    }, options, true);
+	$.fn.stickToTop = function(options) {
+		options = $.extend({
+			scrollParent: window,
+			offset: {top: 0, left: 0},
+			minParentHeight: false,
+			minParentWidth: false,
+			preserveLayout: true,
+			bottomBound: false,
+			onStick: null,
+			onDetach: null
+		}, options, true);
 
-    var scrollParent = options.scrollParent,
-    /*
-    1: BottomBound
-    2: Initial
-    3: Fixed
-    */
-    lastApplied = 0, 
-    parentPosition = $((scrollParent === window) ? scrollParent.document.body : scrollParent).offset();
+		var scrollParent = options.scrollParent,
+		/*
+		1: BottomBound
+		2: Initial
+		3: Fixed
+		*/
+		lastApplied = 0,
+		parentPosition = $((scrollParent === window) ? scrollParent.document.body : scrollParent).offset();
 
-    return $(this).each(function() {
-      var sticky = this,
-      $sticky = $(sticky),
-      initialPosition = $sticky.position(),
-      initialPositioning = $sticky.css('position'),
-      initialWidth = $sticky.width(),
-      stickyHeight = $sticky.outerHeight(true),
-      resizing = false,
-      unsticking = false,
+		return $(this).each(function() {
+			var sticky = this,
+					$sticky = $(sticky),
+					initialPosition = $sticky.position(),
+					initialPositioning = $sticky.css('position'),
+					initialWidth = $sticky.outerWidth(true),
+					initialHeight = $sticky.outerHeight(true),
+					resizing = false,
+					unsticking = false,
+					$layoutDiv,
 
-      fnScrollHandler = function() {
-        var scrollTop = scrollParent.scrollTop || $(document).scrollTop(),
-        parentHeight = ((scrollParent == window) ? window.document.body : scrollParent).offsetHeight,
-        parentWidth = ((scrollParent == window) ? window.document.body : scrollParent).offsetWidth,
-        // If bottomBound, calculate bottom bound including height of the sticky
-        bottomBound = options.bottomBound && (parentHeight - options.bottomBound - stickyHeight),
+			fnScrollHandler = function() {
+				var scrollTop = scrollParent.scrollTop || $(document).scrollTop(),
+				parentHeight = ((scrollParent == window) ? window.document.body : scrollParent).offsetHeight,
+				parentWidth = ((scrollParent == window) ? window.document.body : scrollParent).offsetWidth,
+				// If bottomBound, calculate bottom bound including height of the sticky
+				bottomBound = options.bottomBound && (parentHeight - options.bottomBound - initialHeight),
 
-        applyBottomBound = (!!bottomBound && bottomBound < scrollTop),
+				applyBottomBound = (!!bottomBound && bottomBound < scrollTop),
 
-        applyFixed = (scrollTop >= initialPosition.top - options.offset.top + parentPosition.top),
-        if (options.minParentWidth && parentWidth < options.minParentWidth ) { applyFixed = false; }
-        applyInitial = !applyFixed;
-        
-        
+				applyFixed = (scrollTop >= initialPosition.top - options.offset.top + parentPosition.top),
+				applyInitial = !applyFixed;
 
-        applyFixed = applyFixed && !applyBottomBound;
+				if (options.minParentWidth && parentWidth < options.minParentWidth ) {
+					applyInitial = true;
+					applyFixed = false;
+				}
 
-        if (applyBottomBound && lastApplied !== 1) {
-          var currentPos = $sticky.position();
-          $sticky.css({'position': 'absolute', 'top': bottomBound + 'px' , 'left': currentPos.left + 'px'});
-          lastApplied = 1;
-          if (options.onDetach) {
-            options.onDetach.call(sticky);
-          }
-          return;
-        }
+				applyFixed = applyFixed && !applyBottomBound;
 
-        if ((applyInitial && lastApplied !== 2) ||
-           (options.minParentHeight && parentHeight < options.minParentHeight)) {
-          var props = {'position': initialPositioning};
-          if (initialPositioning !== 'static') {
-            $.extend(props, {'top': initialPosition.top + 'px', 'left': initialPosition.left + 'px'});
-          }
-          $sticky.css(props);
-          lastApplied = 2;
-          if (options.onDetach) {
-            options.onDetach.call(sticky);
-          }
-          return;
-        }
+				// bottom bound
+				if (applyBottomBound && lastApplied !== 1) {
+					var currentPos = $sticky.position();
+					$sticky.css({'position': 'absolute', 'top': bottomBound + 'px' , 'left': currentPos.left + 'px'});
+					lastApplied = 1;
+					if (options.onDetach) {
+						options.onDetach.call(sticky);
+					}
+					return;
+				}
 
-        if (applyFixed && lastApplied !== 3) {
-          $sticky.css({
-            'position':'fixed', 
-            'top': parentPosition.top + (options.offset.top || 0)+'px', 
-            'left': (parentPosition.left + initialPosition.left + (options.offset.left || 0))+'px',
-            'width': initialWidth+'px',
-            'z-index': 1000
-          });
-          lastApplied = 3;
-          if (options.onStick) {
-            options.onStick.call(sticky);
-          }
-          return;
-        }
-      },
-      fnResizeHandler = function(e) { 
-        if (resizing) {
-          return;
-        }
+				// initial
+				if ((applyInitial && lastApplied !== 2) ||
+					(options.minParentHeight && parentHeight < options.minParentHeight)) {
+					var props = {'position': initialPositioning};
+					if (initialPositioning === 'static') {
+						$sticky.removeAttr('style');
 
-        resizing = true;
-        window.setTimeout(function() {
-          if (unsticking) {
-            return;
-          }
+						if ($layoutDiv && parentWidth < options.minParentWidth){
+							$layoutDiv.removeAttr('style');
+						}
+					} else {
+						$.extend(props, {'top': initialPosition.top + 'px', 'left': initialPosition.left + 'px'});
+					}
+					$sticky.css(props);
+					lastApplied = 2;
+					if (options.onDetach) {
+						options.onDetach.call(sticky);
+					}
+					return;
+				}
 
-          var thisPositioning = $sticky.css('position');
-          initialPosition.left = $sticky.css('position', initialPositioning).position().left;
-          $sticky.css('position', thisPositioning);        
-          lastApplied = ''; 
-          fnScrollHandler();
-          resizing = false;
-        }, 50);
-      };
+				// fixed
+				if (applyFixed && lastApplied !== 3) {
+					$sticky.css({
+						'position':'fixed',
+						'top': parentPosition.top + (options.offset.top || 0)+'px',
+						'left': (parentPosition.left + initialPosition.left + (options.offset.left || 0))+'px',
+						'width': initialWidth+'px',
+						'z-index': 1000
+					});
+					lastApplied = 3;
+					if (options.onStick) {
+						options.onStick.call(sticky);
+					}
+					return;
+				}
+			},
+			fnResizeHandler = function(e) {
+				var updatedHeight = initialHeight;
 
-      $(window).on('resize', fnResizeHandler);
+				if (resizing) {
+					return;
+				}
 
-      if (initialPositioning === 'relative') {
-        initialPositioning = 'static';
-      }
+				resizing = true;
+				window.setTimeout(function() {
 
-      $(options.scrollParent).on('scroll', fnScrollHandler);   
+					if (unsticking) {
+						return;
+					}
 
-      // Function to stop stickToTop
-      this.unstickToTop = function() {
-        unsticking = true;
-        $(options.scrollParent).off('scroll', fnScrollHandler);
-        $(window).off('resize', fnResizeHandler);
-      };
-    });
-  };
+					var thisPositioning = $sticky.css('position');
+
+					initialPosition.left = $sticky.css('position', initialPositioning).position().left;
+
+					$sticky.css({
+						position: thisPositioning,
+						width: 'auto'
+					});
+
+					if (options.preserveLayout) {
+						$layoutDiv.css({
+							width: 'auto',
+							height: 'auto'
+						});
+
+						initialWidth = $layoutDiv.outerWidth(true);
+						updatedHeight = $layoutDiv.outerHeight(true);
+
+						// UpdatedHeight will somehow be assigned 0 breaking layout.
+						// Check for this explicitly, and don't assign initialHeight
+						// this value unless it is not 0.
+						if (updatedHeight !==0 ) {
+							initialHeight = updatedHeight;
+						}
+
+						// Update layout div
+						$layoutDiv.css({
+							width: initialWidth + 'px',
+							height: initialHeight + 'px'
+						});
+					}
+
+					$sticky.css({
+						width: initialWidth,
+						height: initialHeight
+					});
+
+					lastApplied = '';
+					fnScrollHandler();
+					resizing = false;
+				}, 50);
+			};
+
+			$(window).on('resize', fnResizeHandler);
+
+			if (initialPositioning === 'relative') {
+				initialPositioning = 'static';
+			}
+
+			$(options.scrollParent).on('scroll', fnScrollHandler);
+
+			if ( options.preserveLayout ) {
+				$layoutDiv = $('<div></div>').css({'height': initialHeight, 'width': initialWidth});
+				$layoutDiv = $sticky.wrap($layoutDiv).parent();
+			}
+
+			// Function to stop stickToTop
+			this.unstickToTop = function() {
+				unsticking = true;
+				$(options.scrollParent).off('scroll', fnScrollHandler);
+				$(window).off('resize', fnResizeHandler);
+			};
+		});
+	};
 
 }(window.document, window.jQuery));
